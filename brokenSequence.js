b44af173729cc5b74db714f17e600b5f49b800f6
silvanagv <silvanagoberdhanvigle@gmail.com>
2017-01-22 19:00:19 -0500

    added broken sequence kata

---
+++ brokenSequence.js
@@ -0,0 +1,7 @@
+// You have a sequence of positive numbers starting with 1, but one number is missing!
+
+// Find out the missing number; if the sequence is not broken, you should return 0. Each sequence always increments by 1.
+
+// In short: an invalid sequence (a string with non numeric character) must return 1, an already complete (or empty) sequence must return 0; a broken sequence with more than one number missing should return the lowest missing number; otherwise return the missing number.
+
+// Note that the input may be with random order.
