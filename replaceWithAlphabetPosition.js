3da0616b4af9e9bc277f957b17ac31acf2e524a8
silvanagv <silvanagoberdhanvigle@gmail.com>
2017-01-22 22:25:22 -0500

    pseudocode replace with alphabet position

--- replaceWithAlphabetPosition.js
+++ replaceWithAlphabetPosition.js
@@ -3,5 +3,16 @@
 // alphabet_position("The sunset sets at twelve o' clock.") "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
 
 function alphabetPosition(text) {
+
+
+  const alphabetString = "abcdefghijklmnopqrstuvwxyz"
+
   return text;
 }
+
+
+
+//generate string with each letter in the alphabet corresponding to a position (obj?)
+//create a new empty string
+//go through each letter in the original string and add a number to the new string based on it's corresponding value
+//if not a letter, ignore it and DON'T return it.
