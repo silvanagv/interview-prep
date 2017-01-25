a7db8a4a308726e49943a38b99e3bc036d288295
silvanagv <silvanagoberdhanvigle@gmail.com>
2017-01-24 19:50:46 -0500

    added css section

--- sumNathanSmithJSQuiz.js
+++ sumNathanSmithJSQuiz.js
@@ -1,7 +1,16 @@
 function sum(){
   var total = 0
   for (i=0;i<arguments.length;i++){
-    total +=arguments[i]
+    if (typeof arguments[i] !== 'number'){
+      if ( typeof parseInt(arguments[i]) === 'number') {
+        var latestNum = parseInt(arguments[i])
+        total+=latestNum
+      }
+    }
+    else {
+      total +=arguments[i]
+
+    }
   }
   return total
 }
@@ -10,10 +19,10 @@ function sum(){
 console.log(sum(1, 2, 3, 4, 5));
 
 // Should equal 0
-sum(5, null, -5);
+console.log(sum(5, null, -5));
 
 // Should equal 10
-sum('1.0', false, 1, true, 1, 'A', 1, 'B', 1, 'C', 1, 'D', 1, 'E', 1, 'F', 1, 'G', 1);
+console.log(sum('1.0', false, 1, true, 1, 'A', 1, 'B', 1, 'C', 1, 'D', 1, 'E', 1, 'F', 1, 'G', 1));
 
 // Should equal 0.3, not 0.30000000000000004
-sum(0.1, 0.2);
+console.log(sum(0.1, 0.2));
