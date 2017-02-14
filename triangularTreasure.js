4485445af04f8e7861ff91f47584174dca589662
silvanagv <silvanagoberdhanvigle@gmail.com>
2017-02-13 23:50:53 -0500

    added new js problem

---
+++ triangularTreasure.js
@@ -0,0 +1,9 @@
+//Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
+
+
+
+//You need to return the nth triangular number. You should return 0 for out of range values:
+triangular(0) //0
+triangular(2) //3
+triangular(3) //6
+triangular(-10) //0
