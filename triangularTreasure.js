3df21bb3c7e68d2537cb24e3b5522f42476fccd5
silvanagv <silvanagoberdhanvigle@gmail.com>
2017-02-14 00:53:04 -0500

    added pseudocode solution

--- triangularTreasure.js
+++ triangularTreasure.js
@@ -1,9 +1,15 @@
 //Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
 
-
-
 //You need to return the nth triangular number. You should return 0 for out of range values:
+
+function triangular( n ) {
+}
+
 triangular(0) //0
 triangular(2) //3
 triangular(3) //6
 triangular(-10) //0
+
+//figure out relationship between n and the number of dots required
+//make solution using this pattern
+//take care of edge cases (negative numbers, 0?)
