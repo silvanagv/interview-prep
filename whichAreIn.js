6069b1dcc6c1e982bb3ed534e8bbc45eb7ebd86f
silvanagv <silvanagoberdhanvigle@gmail.com>
2017-02-20 23:43:37 -0500

    pseudocoded answer

---
+++ whichAreIn.js
@@ -0,0 +1,6 @@
+// 1. check if each item is in any item of the other array as a substring
+// 2. If it is, push it into a new array
+// 3. Sort the new array alphabetically
+// 4. Check for duplicates
+// 5. If no matches, return empty array
+// Edge cases?
