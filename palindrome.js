3af67e388fdf3b60cbdd0a94e0c5632aa7717dd3
silvanagv <silvanagoberdhanvigle@gmail.com>
2017-03-29 11:52:06 -0400

    palindrome v1

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+function palindrome(s){
+    let p = ""
+    for(let i = s.length-1; i >= 0; i--){
+      p+=s[i]
+    }
+    if (p === s){
+      return true
+    }
+    else {
+      return false
+    }
+}
+
+console.log(palindrome("otto"))
